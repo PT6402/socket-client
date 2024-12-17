7c9a486fdec027f4f1f0eedf23ac85bc3b44b440
Phát Trần <tranphat701@gmail.com>
2024-12-17 18:57:50 +0700

    update

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 export default function HomePage() {
   const navigate = useNavigate();
   const handleSend = () => {
-    axios.get("http://localhost:8080/send").then(() => {
+    axios.get("https://socket-server-mur9.onrender.com/send").then(() => {
       navigate("/collection");
     });
   };
